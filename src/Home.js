a35bde8f831674598fb79dfe8980528bbf679c73
prem <premkumargunasegaran>
2023-08-30 05:32:50 -0400

    Login omplted

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate,Link } from "react-router-dom";
 
 function Home() {
-  return <div>Dashboard</div>;
+  const navigate = useNavigate();
+ 
+  useEffect(()=>{
+    let username =  sessionStorage.getItem("username")
+      if(username === "" || username === null){
+        navigate("/login")
+      }
+     
+  },[])
+  return <div className="">
+    
+  <div className=" header bg-primary ">
+    <div className="container d-flex header bg-primary justify-content-between py-2">
+    <h1 className="text-white">Logo</h1> 
+    <button className="bg-success btn text-white"><Link to="/login" className="text-white text-decoration-none">Logout</Link></button>
+    </div>
+  
+  </div>
+  <h2 className="mt-2">Dashboard</h2>
+  </div>;
 }
 
 export default Home;
--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,18 @@
 import { React, useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
 
 function Login() {
+  const [email , setEmail] = useState("") 
   const [logindata, setlogindata] = useState({
     email: "",
     password: "",
   });
+  useEffect(()=>{
+   sessionStorage.clear();
+  },[])
+ const navigate = useNavigate("/")
   const handlechange = (e) => {
     const { name, value } = e.target;
     setlogindata({
@@ -17,13 +22,36 @@ function Login() {
   };
   const handlesubmit = (e) => {
     e.preventDefault();
+    // console.log(email);
+   
+    
     if (validate()) {
-      console.log(logindata);
+      axios.get(`http://localhost:8000/user?email=${logindata.email}` ).then((res)=>{
+       console.log(res.data);
+      if(res.data.length=== 0){
+        toast.error("Please Enter Valid user");
+       
+      }
+      else {
+        const {email, password} = res.data[0]
+        if(password === logindata.password){
+          toast.success("login success");
+          sessionStorage.setItem("username", email);
+          navigate("/");
+        }
+        else{
+          toast.error("Please Enter Valid Password");
+        }
+      }
+      
+      }).catch((err)=>{
+        console.log("error", err);
+      })
     }
   };
   const validate = () => {
     let isvalid = true;
-    if (logindata.email === "") {
+    if (logindata.email == "") {
       toast.error("Enter Proper Mail ID");
       isvalid = false;
     }
